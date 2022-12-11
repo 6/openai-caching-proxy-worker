1ab74a905096604d35512ca7d26c79c21fd06c08
Peter Graham <peterghm@gmail.com>
2022-12-10 22:50:23 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,6 @@ export interface Env {
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response('Hello World!');
+    return new Response('hola!');
   },
 };
