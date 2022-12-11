8a16d45b0de3b3a0adfd0655c09994eec1da63fb
Peter Graham <peterghm@gmail.com>
2022-12-10 23:19:05 -0500

    start working on proxy

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
   // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
   // MY_KV_NAMESPACE: KVNamespace;
@@ -21,6 +11,70 @@ export interface Env {
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response('hola!');
+    const { pathname } = new URL(request.url);
+    const ttl = request.headers.get('X-Proxy-TTL');
+
+    const isProxyRequest =
+      request.method === 'POST' &&
+      request.headers.get('content-type') === 'application/json' &&
+      ttl !== '0';
+
+    if (isProxyRequest) {
+      return await handleProxy({ request, env, ctx, ttl, pathname });
+    }
+
+    // Fallback response when not proxying (healthcheck)
+    return new Response(JSON.stringify({ ok: true }), {
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+    });
   },
+};
+
+interface HandleProxyOpts {
+  request: Request;
+  env: Env;
+  ctx: ExecutionContext;
+  ttl: string | null;
+  pathname: string;
+}
+const handleProxy = async ({
+  request,
+  env,
+  ctx,
+  ttl,
+  pathname,
+}: HandleProxyOpts): Promise<Response> => {
+  const cacheKey = 'todo';
+  const cachedResponse = await getCachedResponse({ cacheKey });
+  if (cachedResponse) {
+    console.log('Returning cached response.');
+    return cachedResponse;
+  }
+
+  console.log('No cached response found. Proxying and caching response instead.');
+
+  const fetchUrl = `https://api.openai.com/v1${pathname.replace(/^\/proxy/, '')}`;
+
+  const fetchHeaders: Record<string, string> = {};
+  for (const [key, value] of request.headers.entries()) {
+    fetchHeaders[key] = value;
+  }
+
+  const response = await fetch(fetchUrl, {
+    method: request.method,
+    headers: fetchHeaders,
+    body: request.body,
+  });
+
+  return response;
+};
+
+const getCachedResponse = async ({ cacheKey }: { cacheKey: string }): Promise<Response | null> => {
+  // for (const [key, value] of Object.entries(cachedResponse.headers)) {
+  // 	res.setHeader(key, value);
+  // }
+  // return res.status(cachedResponse.status).send(cachedResponse.body);
+  return null;
 };
